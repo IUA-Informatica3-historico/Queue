09ed22159a6e872560719403433cb16a7cce6033
Ricardo Martin Marcucci <mmarcucci@ucc.edu.ar>
2017-09-07 09:19:45 -0300

    * Se agrego el metodo poner * Se corrigio clase nodo

--- src/Nodo.java
+++ src/Nodo.java
@@ -1,5 +1,6 @@
 public class Nodo<T> {
     private T dato;
+    private Nodo<T> next;
 
     public T getDato() {
         return dato;
@@ -9,13 +10,20 @@ public class Nodo<T> {
         this.dato = dato;
     }
 
-    public Nodo getNext() {
+    public Nodo<T> getNext() {
         return next;
     }
 
-    public void setNext(Nodo next) {
+    public void setNext(Nodo<T> next) {
         this.next = next;
     }
 
-    private Nodo next;
+    public Nodo(T dato, Nodo<T> next) {
+        this.dato = dato;
+        this.next = next;
+    }
+
+    public Nodo(T dato) {
+        this.dato = dato;
+    }
 }
--- src/Queue.java
+++ src/Queue.java
@@ -1,4 +1,21 @@
-public class Queue {
+public class Queue<T> {
+    private Nodo<T> frente;
+    private Nodo<T> fondo;
+
+    public Queue() {
+        frente = null;
+        fondo = null;
+    }
+
+    public void poner(T dato) {
+        if (null == fondo) {
+            fondo = new Nodo<T>(dato, null);
+            frente = fondo;
+        } else {
+            fondo.setNext(new Nodo<T>(dato, null));
+            fondo = fondo.getNext();
+        }
+    }
 
 
 }
